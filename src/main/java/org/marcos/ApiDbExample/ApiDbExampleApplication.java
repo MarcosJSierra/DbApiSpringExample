dc0ab55f85dffb6ba6acd5855387e913b572a816
Marcos Sierra <msierra@bdgsa.net>
2023-07-19 17:04:37 -0600

    Agregando proyecto base

---
+++ src/main/java/org/marcos/ApiDbExample/ApiDbExampleApplication.java
@@ -0,0 +1,13 @@
+package org.marcos.ApiDbExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDbExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDbExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/marcos/ApiDbExample/ApiDbExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.marcos.ApiDbExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDbExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
